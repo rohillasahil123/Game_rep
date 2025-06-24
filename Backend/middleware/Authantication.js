b42b64ac92f5af41f8f6a1931462aab5e1049193
rohillasahil123 <rohillasahil704@gmail.com>
2025-06-24 13:53:16 +0530

    start

--- Backend/middleware/Authantication.js
+++ Backend/middleware/Authantication.js
@@ -7,7 +7,7 @@ const authenticateToken = async (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ message: "No token provided" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, "jaibabaki");
     const user = await User.findById(decoded.id).select("-password");
     if (!user) return res.status(401).json({ message: "User not found" });
 
