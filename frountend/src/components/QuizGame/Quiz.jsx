d52350dc13cbd72a2bf23b7db5a40629a1517a66
rohillasahil123 <rohillasahil704@gmail.com>
2025-06-24 22:30:50 +0530

    question

--- frountend/src/components/QuizGame/Quiz.jsx
+++ frountend/src/components/QuizGame/Quiz.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import useContestStore from "../../Store/useContestStore";
+const Api_URL = "https//foodenergy.shop"
 
 
 
